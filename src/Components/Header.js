83acdb2df7283a5792d8453a1fd512e4deb114de
headphones06 <headphones.CG@gmail.com>
2022-09-26 15:44:55 +0900

    ホームページ完成

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../Style/Header.css";
+import "../Style/Header.scss";
 
 export function Header() {
   return (
     <dev>
-      <header>自己紹介HP</header>
+      <header>
+        <p className="headerTitle">自己紹介HP</p>
+        <ul className="headerMenu">
+          <li className="headerMenu__item">
+            <Link to={"/"}>HOME</Link>
+          </li>
+          <li className="headerMenu__item">
+            <Link to={"/info"}>自己紹介</Link>
+          </li>
+          <li className="headerMenu__item">
+            <Link to={"/works"}>作品集</Link>
+          </li>
+        </ul>
+      </header>
     </dev>
   );
 }
